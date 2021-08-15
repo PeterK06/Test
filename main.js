9cd4fb1092775d2630e08672ed4903ef20f055b7
Petrine51 <kerrpeter52@gmail.com>
2021-08-16 09:40:43 +1000

    a

---
+++ main.js
@@ -0,0 +1,14 @@
+function enlargeImg(img) {
+    img.style.transform = "scale(100)";
+    img.style.transition = "transform 2s ease";
+}
+
+function redirect(body) {
+    body.style.transform = "scale(0)";
+    body.style.transition = "transform 0.2s ease";
+}
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
